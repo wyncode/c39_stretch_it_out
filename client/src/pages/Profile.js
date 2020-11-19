843f8e75c3d52c8d05099c158bf338935669033f
Love <la969@mynsu.nova.email>
2020-11-19 18:21:45 -0500

    additions2

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,7 +3,11 @@ import { Container, Image, Button } from 'react-bootstrap';
 import { AppContext } from '../context/AppContext';
 import axios from 'axios';
 import swal from 'sweetalert';
+
 import Navigation from '../components/Navigation';
+
+import Navbar from '../components/Navbar';
+
 
 const Profile = ({ history: { push } }) => {
   const { currentUser, setCurrentUser, setLoading } = useContext(AppContext);
@@ -74,7 +78,11 @@ const Profile = ({ history: { push } }) => {
   };
   return (
     <>
+
       <Navigation />
+
+      <Navbar />
+
       <Container
         className="d-flex justify-content-center align-items-center
          flex-column"
