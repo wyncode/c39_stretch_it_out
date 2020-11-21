130489b553eabc68cac034222dfbbb556ba128d3
Love <la969@mynsu.nova.email>
2020-11-21 12:43:46 -0500

    Navbar updated

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,40 +7,41 @@ import Logout from './Logout';
 const Navbar = () => {
   const { currentUser } = useContext(AppContext);
 
-  return;
-  <Navbar bg="light" expand="lg">
-    <Navbar.Brand as={Link} to="/">
-      Find a Stretch
-    </Navbar.Brand>
-    <Navbar.toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Item className="mr-2">Stretches</Nav.Item>
-        <Nav.Item>Benefits</Nav.Item>
-        <Nav.Item>Blog</Nav.Item>
-        <Nav.Item>Log In</Nav.Item>
-        <Nav.Item>
-          <Dropdown drop="down" className="mr-1">
-            <Dropdown.Toggle variant="">
-              <Image
-                scr={currentUser?.avatar ? currentUser.avatar : 'image.jpg'}
-                height={50}
-                width={50}
-                roundedCircle
-              />
-            </Dropdown.Toggle>
-            <Dropdown.menu>
-              <Dropdown.Item as={Link} to="/profile">
-                Profile
-              </Dropdown.Item>
-              <Logout />
-            </Dropdown.menu>
-          </Dropdown>
-        </Nav.Item>
-        <Nav.Item>Start Trial</Nav.Item>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>;
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand as={Link} to="/">
+        Find a Stretch
+      </Navbar.Brand>
+      <Navbar.toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Item className="mr-2">Stretches</Nav.Item>
+          <Nav.Item>Benefits</Nav.Item>
+          <Nav.Item>Blog</Nav.Item>
+          <Nav.Item>Log In</Nav.Item>
+          <Nav.Item>
+            <Dropdown drop="down" className="mr-1">
+              <Dropdown.Toggle variant="">
+                <Image
+                  scr={currentUser?.avatar ? currentUser.avatar : 'image.jpg'}
+                  height={50}
+                  width={50}
+                  roundedCircle
+                />
+              </Dropdown.Toggle>
+              <Dropdown.menu>
+                <Dropdown.Item as={Link} to="/profile">
+                  Profile
+                </Dropdown.Item>
+                <Logout />
+              </Dropdown.menu>
+            </Dropdown>
+          </Nav.Item>
+          <Nav.Item>Start Trial</Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
 
 export default Navbar;
