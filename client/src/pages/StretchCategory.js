595834b462a9dac07a4017278c9c3f16e3502590
ljgalaty <laurel.galaty@gmail.com>
2020-12-10 17:57:41 -0500

    changed hover properties and  box shadow on all stretch cards, and fixed stretch banner

--- client/src/pages/StretchCategory.js
+++ client/src/pages/StretchCategory.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Navigation from '../components/navigation/Navigation';
 import CategoryCard from '../components/finding-a-stretch/CategoryCard';
-import { StretchBanner } from '../stretchimages/index';
+import { BlueBanner } from '../stretchimages/index';
 import ContactFooter from './anon-home-page/components/ContactFooter';
 import GetStarted from './anon-home-page/components/GetStartedFooter';
 
@@ -10,7 +10,10 @@ const StretchCategory = () => {
   return (
     <div>
       <Navigation />
-      <img src={StretchBanner} className="find-your-stretch" />
+      <div className="stretch-banner-div">
+        <img src={BlueBanner} className="stretch-banner" />
+        <div className="find-your-stretch">Find Your Stretch</div>
+      </div>
       <Container>
         <CategoryCard />
       </Container>
